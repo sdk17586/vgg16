075694454de577f0118fdeb41425bd22ea3050ea
sdk17586 <122353362+sdk17586@users.noreply.github.com>
2023-06-10 13:46:22 +0900

    Delete vgg16 directory

--- vgg16/bin/utils.py
+++
@@ -1,8 +0,0 @@
-def calculateAccuracy(yPred, label):
-    
-    topPred = yPred.argmax(1, keepdim=True)
-    correct = topPred.eq(label.view_as(topPred)).sum()
-    accuracy = correct.float() / label.shape[0]
-
-    return accuracy
-
